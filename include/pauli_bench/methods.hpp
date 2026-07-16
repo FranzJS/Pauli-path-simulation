ce70e3857e613d15669ece42d7d6845b766af710
FranzJS <72516131+FranzJS@users.noreply.github.com>
2026-07-16 23:10:01 +0200

    Remove obsolete multi-method interface

--- include/pauli_bench/methods.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "types.hpp"
-namespace pb {
-Result run_statevector(const Circuit&, double budget_s);
-Result run_exact_sparse(const Circuit&, double budget_s, std::uint64_t term_cap=500000);
-Result run_dfs(const Circuit&, double budget_s, std::uint64_t path_cap=200000000);
-Result run_monte_carlo(const Circuit&, double budget_s, std::uint64_t max_samples=1000000, std::uint64_t seed=7);
-}
